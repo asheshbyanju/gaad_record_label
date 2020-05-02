598a002d47f0e374461950f0d23246993bb4a23a
Ashesh Byanju <byanju.ashesh@gmail.com>
2020-05-02 13:26:02 -0400

    added home page

--- music_webapp/music_webapp/settings.py
+++ music_webapp/music_webapp/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'gaad_record_label',
 ]
 
 MIDDLEWARE = [
--- music_webapp/music_webapp/urls.py
+++ music_webapp/music_webapp/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from gaad_record_label import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index, name="home-page"),
 ]
