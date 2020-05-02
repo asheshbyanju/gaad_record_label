f3a1cb1f32381077723ff633a43c35c25e96a217
Ashesh Byanju <byanju.ashesh@gmail.com>
2020-05-02 11:43:16 -0400

    initial project setup

---
+++ music_webapp/gaad_record_label/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class GaadRecordLabelConfig(AppConfig):
+    name = 'gaad_record_label'
---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/asheshbyanju/PycharmProjects/gaad_record_label/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
