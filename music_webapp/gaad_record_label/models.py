9a23f7ed6b8193d74b2dea66e5d502e2717a553a
Ashesh Byanju <byanju.ashesh@gmail.com>
2020-05-02 21:44:22 -0400

    database added, html fixed, artist table added

--- music_webapp/gaad_record_label/models.py
+++ music_webapp/gaad_record_label/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Artist(models.Model):
+    name = models.CharField(max_length=100)
+    about = models.TextField()
+    nationality = models.CharField(max_length= 50)
+    location = models.CharField(max_length=100)
+    genre = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='pics')
