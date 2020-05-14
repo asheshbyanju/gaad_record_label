d5d1dad8810a8be59f1f400fd95b07d405f58fbd
Ashesh Byanju <byanju.ashesh@gmail.com>
2020-05-13 20:08:13 -0400

    adding django-heroku and psycopg

--- music_webapp/music_webapp/settings.py
+++ music_webapp/music_webapp/settings.py
@@ -148,3 +148,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+# Configure Django App for Heroku.
+import django_heroku
+django_heroku.settings(locals())
