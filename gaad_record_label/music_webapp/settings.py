f569e7fbfcf1f7cf0eec4bc3ae90baf58be42351
Ashesh Byanju <byanju.ashesh@gmail.com>
2020-05-14 00:44:58 -0400

    changing for rename

--- gaad_record_label/music_webapp/settings.py
+++ gaad_record_label/music_webapp/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'gaad_record_label.apps.GaadRecordLabelConfig',
+    'gaad_record_label',
 ]
 
 MIDDLEWARE = [
