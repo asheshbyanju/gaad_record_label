12e79bd867fecf26c046fc93e28247f4539ef285
Ashesh Byanju <byanju.ashesh@gmail.com>
2020-05-14 00:56:09 -0400

    adding urls to gaad

--- gaad_record_label/gaad_record_label/urls.py
+++ gaad_record_label/gaad_record_label/urls.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 
 urlpatterns = [
-path('admin/', admin.site.urls),
-    path('', include('gaad_record_label.urls')),
-    path('accounts/', include('accounts.urls'))
+    path('admin/', admin.site.urls),
+    # path('', include('gaad_record_label.urls')),
+    path('accounts/', include('accounts.urls')),
     path('', views.index, name="home"),
     path('artist/<int:artist_id>/', views.artist_details, name="artist_detail"),
     path('artist/', views.artist_list, name="artist_list")
